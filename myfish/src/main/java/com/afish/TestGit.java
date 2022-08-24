ebdb09196de682f1cf1eaa183c566625e2e39e35
chuck <aaa@126.com>
2022-08-25 00:25:42 +0800

    c2

--- myfish/src/main/java/com/afish/TestGit.java
+++ myfish/src/main/java/com/afish/TestGit.java
@@ -1,7 +1,7 @@
 package com.afish;
 
 public class TestGit {
-    public static void main(string args[]){
+    public static void main(String[] args){
         System.out.println("hello world!");
     }
 }
